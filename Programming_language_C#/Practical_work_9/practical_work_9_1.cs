33eda44b2031ec290d7393963cbdea1ff3de3fe8
Oleksandr Nesterenko <oleksandrnesterenko1289@gmail.com>
2026-04-28 14:34:21 +0300

    Create Practical_work_9

---
+++ Programming_language_C#/Practical_work_9/practical_work_9_1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace practical_work_9_1
+{
+    class Transport                                             // базовий клас транспорт
+    {
+        protected double netProfit;                             // чистий прибуток транспорту
+
+        public virtual void inputData()                         // віртуальний метод для введення даних
+        {
+            Console.Write("  Введіть чистий прибуток у гривнях: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // якщо вдалося перетворити текст на double
+                if (double.TryParse(inputStr, out netProfit))
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести дійсне число\n  Спробуйте ще раз: ");
+            }
+        }
+
+        public virtual void outputData()                        // віртуальний метод для виведення даних
+        {
+            Console.WriteLine("  Чистий прибуток: {0} грн", netProfit);
+        }
+    }
+
+    class FreightTransport : Transport                          // похідний клас вантажний транспорт, який наслідує базовий клас Transport
+    {
+        string type;                                            // тип вантажного транспорту
+        string model;                                           // модель вантажного транспорту
+        double loadCapacity;                                    // вантажопідйомність
+
+        public override void inputData()                        // перевизначений метод для заповнення даних
+        {
+            // введення типу транспорту
+            Console.Write("  Введіть тип вантажного транспорту (автомобіль/поїзд/літак/корабель/інше): ");
+            type = Console.ReadLine();
+
+            // введення моделі транспорту
+            Console.Write("  Введіть модель вантажного транспорту типу {0}: ", type);
+            model = Console.ReadLine();
+
+            // введення вантажопідйомності
+            Console.Write("  Введіть вантажопідйомність у тоннах: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // якщо вдалося перетворити текст на double та число в межах діапазону
+                if (double.TryParse(inputStr, out loadCapacity) && loadCapacity >= 1 && loadCapacity <= 1000000)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести дійсне число в межах від 1 до 1000000\n  Спробуйте ще раз: ");
+            }
+
+            // виклик методу базового класу для введення чистого прибутку
+            base.inputData();
+        }
+
+        public override void outputData()                       // перевизначений метод для виведення інформації
+        {
+            Console.WriteLine("  Тип вантажного транспорту: {0}", type);
+            Console.WriteLine("  Модель вантажного транспорту: {0}", model);
+            Console.WriteLine("  Вантажопідйомність: {0} т", loadCapacity);
+            Console.WriteLine("  Чистий прибуток: {0} грн", netProfit);
+        }
+    }
+
+    class PassengerTransport : Transport                        // похідний клас пасажирський транспорт, який наслідує базовий клас Transport
+    {
+        string type;                                            // тип пасажирського транспорту
+        string model;                                           // модель пасажирського транспорту
+        int numberSeats;                                        // кількість місць для пасажирів
+
+        public override void inputData()                        // перевизначений метод для заповнення даних
+        {
+            // введення типу транспорту
+            Console.Write("  Введіть тип пасажирського транспорту (автомобіль/поїзд/літак/корабель/інше): ");
+            type = Console.ReadLine();
+
+            // введення моделі транспорту
+            Console.Write("  Введіть модель пасажирського транспорту типу {0}: ", type);
+            model = Console.ReadLine();
+
+            // введення кількості місць
+            Console.Write("  Введіть кількість місць для пасажирів: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // якщо вдалося перетворити текст на int та число в межах діапазону
+                if (int.TryParse(inputStr, out numberSeats) && numberSeats >= 1 && numberSeats <= 10000)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести ціле число в межах від 1 до 10000\n  Спробуйте ще раз: ");
+            }
+
+            // виклик методу базового класу для введення чистого прибутку
+            base.inputData();
+        }
+
+        public override void outputData()                       // перевизначений метод для виведення інформації
+        {
+            Console.WriteLine("  Тип пасажирського транспорту: {0}", type);
+            Console.WriteLine("  Модель пасажирського транспорту: {0}", model);
+            Console.WriteLine("  Кількість місць: {0}", numberSeats);
+            Console.WriteLine("  Чистий прибуток: {0} грн", netProfit);
+        }
+    }
+
+    internal class Program                                      // головний клас програми
+    {
+        static void Main(string[] args)                         // головна функція програми
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // встановлення кодування UTF-8 для коректного відображення кирилиці в консолі
+
+            Console.WriteLine("Введіть інформацію про вантажний транспортний засіб:");
+            FreightTransport freightTransport = new FreightTransport(); // створення об'єкта класу вантажний транспорт
+            freightTransport.inputData();                       // виклик методу заповнення даних для вантажного транспорту
+
+            Console.WriteLine("\nВведіть інформацію про пасажирський транспортний засіб:");
+            PassengerTransport passengerTransport = new PassengerTransport(); // створення об'єкта класу пасажирський транспорт
+            passengerTransport.inputData();                     // виклик методу заповнення даних для пасажирського транспорту
+
+            Console.WriteLine("\nВведена інформація про вантажний транспортний засіб:");
+            freightTransport.outputData();                      // виведення інформації про створений вантажний транспорт
+
+            Console.WriteLine("\nВведена інформація про пасажирський транспортний засіб:");
+            passengerTransport.outputData();                    // виведення інформації про створений пасажирський транспорт
+        }
+    }
+}
---
+++ Programming_language_C#/Practical_work_9/practical_work_9_2.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace practical_work_9_2
+{
+    class InvestmentAsset                                       // базовий клас інвестиційний актив
+    {
+        protected string ticker;                                // тікер (назва) активу, наприклад VOO
+        protected double currentPrice;                          // поточна ціна за одиницю
+        protected double investedAmount;                        // загальна вкладена сума
+
+        public virtual void inputData()                         // віртуальний метод для введення даних
+        {
+            // введення назви активу
+            Console.Write("  Введіть назву (тікер) активу: ");
+            ticker = Console.ReadLine();
+
+            // введення поточної ціни активу
+            Console.Write("  Введіть поточну ціну активу у доларах: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // перевірка на дійсне число більше нуля
+                if (double.TryParse(inputStr, out currentPrice) && currentPrice > 0)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести додатне число\n  Спробуйте ще раз: ");
+            }
+
+            // введення вкладеної суми
+            Console.Write("  Введіть загальну вкладену суму у доларах: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // перевірка на дійсне число більше нуля
+                if (double.TryParse(inputStr, out investedAmount) && investedAmount > 0)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести додатне число\n  Спробуйте ще раз: ");
+            }
+        }
+
+        public virtual void outputData()                        // віртуальний метод для виведення даних
+        {
+            Console.WriteLine("  Тікер активу: {0}", ticker);
+            Console.WriteLine("  Поточна ціна: {0} $", currentPrice);
+            Console.WriteLine("  Вкладена сума: {0} $", investedAmount);
+        }
+    }
+
+    class ETF : InvestmentAsset                                 // похідний клас індексний фонд, який наслідує InvestmentAsset
+    {
+        double managementFee;                                   // комісія фонду за управління
+        double dividendYield;                                   // дивідендна дохідність
+
+        public override void inputData()                        // перевизначений метод для заповнення даних
+        {
+            base.inputData();                                   // виклик методу базового класу
+
+            // введення комісії фонду
+            Console.Write("  Введіть комісію фонду за управління у відсотках: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // перевірка на дійсне число в межах від 0 до 10%
+                if (double.TryParse(inputStr, out managementFee) && managementFee >= 0 && managementFee <= 10)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести число від 0 до 10\n  Спробуйте ще раз: ");
+            }
+
+            // введення дивідентної дохідності
+            Console.Write("  Введіть річну дивідендну дохідність у відсотках: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // перевірка на дійсне число в межах від 0 до 100%
+                if (double.TryParse(inputStr, out dividendYield) && dividendYield >= 0 && dividendYield <= 100)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести число від 0 до 100\n  Спробуйте ще раз: ");
+            }
+        }
+
+        public override void outputData()                       // перевизначений метод для виведення інформації
+        {
+            base.outputData();                                  // виклик методу базового класу
+
+            Console.WriteLine("  Комісія фонду: {0}%", managementFee);
+            Console.WriteLine("  Дивідендна дохідність: {0}%", dividendYield);
+
+            // розрахунок чистого річного прибутку з урахуванням комісії
+            double netReturn = investedAmount * ((dividendYield - managementFee) / 100);
+            Console.WriteLine("  Очікуваний річний прибуток: {0} $", netReturn);
+        }
+    }
+
+    class Bond : InvestmentAsset                                // похідний клас державна облігація, який наслідує InvestmentAsset
+    {
+        double interestRate;                                    // річна відсоткова ставка
+        int maturityYears;                                      // термін погашення (у роках)
+
+        public override void inputData()                        // перевизначений метод для заповнення даних
+        {
+            base.inputData();                                   // виклик методу базового класу
+
+            // введення відсоткової ставки
+            Console.Write("  Введіть річну відсоткову ставку у відсотках: ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // перевірка на дійсне число в межах від 0 до 100%
+                if (double.TryParse(inputStr, out interestRate) && interestRate >= 0 && interestRate <= 100)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести число від 0 до 100\n  Спробуйте ще раз: ");
+            }
+
+            // введення терміну погашення
+            Console.Write("  Введіть термін погашення (у роках): ");
+            while (true)                                        // цикл, який триватиме поки не спрацює break
+            {
+                string inputStr = Console.ReadLine();           // зчитування рядка з консолі
+
+                // перевірка на ціле число в межах від 1 до 50 років
+                if (int.TryParse(inputStr, out maturityYears) && maturityYears >= 1 && maturityYears <= 50)
+                {
+                    break;
+                }
+
+                Console.Write("  Помилка! Потрібно ввести ціле число від 1 до 50\n  Спробуйте ще раз: ");
+            }
+        }
+
+        public override void outputData()                       // перевизначений метод для виведення інформації
+        {
+            base.outputData();                                  // виклик методу базового класу
+
+            Console.WriteLine("  Річна ставка: {0}%", interestRate);
+            Console.WriteLine("  Термін погашення: {0} років", maturityYears);
+
+            // розрахунок прибутку за весь період дії облігації
+            double totalReturn = investedAmount * (interestRate / 100) * maturityYears;
+            Console.WriteLine("  Очікуваний прибуток за весь період: {0} $", totalReturn);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // встановлення кодування UTF-8 для коректного відображення кирилиці в консолі
+
+            Console.WriteLine("Введіть інформацію про акцію індексного фонду (ETF):");
+            ETF myETF = new ETF();                              // створення об'єкта класу ETF
+            myETF.inputData();                                  // виклик методу заповнення даних для ETF
+
+            Console.WriteLine("\nВведіть інформацію про державну облігацію:");
+            Bond myBond = new Bond();                           // створення об'єкта класу Bond
+            myBond.inputData();                                 // виклик методу заповнення даних для облігації
+
+            Console.WriteLine("\nВведена інформація про індексний фонд (ETF):");
+            myETF.outputData();                                 // виведення інформації про ETF
+
+            Console.WriteLine("\nВведена інформація про державну облігацію:");
+            myBond.outputData();                                // виведення інформації про облігацію
+        }
+    }
+}
