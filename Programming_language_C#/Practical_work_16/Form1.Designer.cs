4134d3f1ae10633e97ae0ec6c3978b2a907e503c
Oleksandr Nesterenko <oleksandrnesterenko1289@gmail.com>
2026-05-17 19:52:48 +0300

    Create Practical_work_16

---
+++ Programming_language_C#/Practical_work_16/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace practical_work_16_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(99, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(578, 357);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 394);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(789, 47);
+            this.panel1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(601, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 39);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Очистити";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(303, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 39);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Зберегти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Огляд";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Programming_language_C#/Practical_work_16/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace practical_work_16_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()                                                              // конструктор класу форми
+        {
+            InitializeComponent();                                                  // ініціалізація компонентів форми
+        }
+
+        Pen pen1 = new Pen(Color.Red, 4);                                           // створення червоного пензлика товщиною 4
+        Bitmap bmp;                                                                 // оголошення змінної для зберігання зображення
+        bool permit;                                                                // дозвіл на малювання
+        Point startPoint;                                                           // координати початкової точки для малювання лінії
+
+        string full_name_of_image;                                                  // змінна для зберігання шляху до файлу
+
+        private void Form1_Load(object sender, EventArgs e)                         // обробник події завантаження форми
+        {
+            this.Text = "Редагування картинки";
+            button1.Text = "Додати фото";
+            button2.Text = "Зберегти";
+            button3.Text = "Очистити";
+        }
+
+        private void button1_Click(object sender, EventArgs e)                      // обробник події натискання на кнопку "Огляд"
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();                      // створення діалогового вікна для відкриття файлу
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)                        // якщо користувач обрав файл і натиснув "ОК"
+            {
+                try                                                                 // блок перехоплення помилок
+                {
+                    full_name_of_image = open_dialog.FileName;                      // збереження шляху файлу
+                    bmp = new Bitmap(open_dialog.FileName);                         // завантаження зображення
+                    this.pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;        // налаштування розміру PictureBox під оригінальний розмір картинки
+                    pictureBox1.Image = bmp;                                        // відображення картинки у компоненті PictureBox
+                    pictureBox1.Invalidate();                                       // оновлення екрану
+                }
+                catch                                                               // якщо виникла помилка під час відкриття
+                {
+                    MessageBox.Show("Обраний файл неможливо відкрити", "Увага", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)                      // обробник події натискання на кнопку "Зберегти"
+        {
+            if (pictureBox1.Image != null)                                          // перевірка чи завантажено зображення на форму
+            {
+                SaveFileDialog savedialog = new SaveFileDialog();                   // створення діалогового вікна для збереження файлу
+
+                savedialog.Title = "Зберегти як ...";
+                savedialog.OverwritePrompt = true;
+                savedialog.ShowHelp = true;
+
+                savedialog.Filter = "Зображення JPEG (*.jpg)|*.jpg|Всі файли (*.*)|*.*"; // налаштування форматів для збереження
+                savedialog.DefaultExt = "jpg";                                      // задання розширення файлу за замовчуванням
+
+                if (savedialog.ShowDialog() == DialogResult.OK)                     // якщо користувач вказав шлях і натиснув "Зберегти"
+                {
+                    try                                                             // блок перехоплення помилок
+                    {
+                        bmp.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg); // збереження малюнка у форматі JPEG
+                    }
+                    catch                                                           // якщо виникла помилка під час збереження
+                    {
+                        MessageBox.Show("Неможливо зберегти файл", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)                      // обробник події натискання на кнопку "Очистити"
+        {
+            Graphics g = pictureBox1.CreateGraphics();                              // створення об'єкта
+            g.Clear(SystemColors.Window);                                           // зафарбовування
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)         // обробник події руху курсора миші по картинці
+        {
+            if (e.Button == MouseButtons.Left)                                      // перевірка чи затиснута саме ліва кнопка миші
+            {
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))          // створення об'єкта для малювання по картинці
+                {
+                    if (permit == true)                                             // якщо малювання дозволено 
+                    {
+                        // малюємо
+                        g.DrawLine(pen1, startPoint, e.Location);
+                        startPoint = e.Location;
+                        pictureBox1.Invalidate();
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)         // обробник події натискання кнопки миші на картинці
+        {
+            if (e.Button == MouseButtons.Left)                                      // перевірка чи натиснута саме ліва кнопка
+            {
+                permit = true;                                                      // надання дозволу на малювання
+                startPoint = e.Location;                                            // фіксація початкової координати курсору
+            }
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)           // обробник події відпускання кнопки миші
+        {
+            if (e.Button == MouseButtons.Left)                                      // перевірка чи була відпущена саме ліва кнопка
+            {
+                permit = false;                                                     // скасування дозволу на малювання
+            }
+        }
+    }
+}
