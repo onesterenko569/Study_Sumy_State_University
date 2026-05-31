90d3a20c26388cd43ebdcd62c3a402a9b3084ed3
Oleksandr Nesterenko <oleksandrnesterenko1289@gmail.com>
2026-05-31 14:25:57 +0300

    Add practical work 20

---
+++ Programming_language_C#/Practical_work_20/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace practical_work_20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Пошук";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Forward";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(317, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(462, 29);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(123, 41);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Home";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(604, 29);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 41);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Save as...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(755, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "youtube.com",
+            "google.com",
+            "Gmail.com"});
+            this.comboBox1.Location = new System.Drawing.Point(966, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(250, 28);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Вибране";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(26, 99);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1190, 473);
+            this.webBrowser1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 584);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Programming_language_C#/Practical_work_20/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace practical_work_20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()                                                                      // конструктор класу форми
+        {
+            InitializeComponent();                                                          // ініціалізація компонентів форми
+        }
+
+        private void Form1_Load(object sender, EventArgs e)                                 // обробник події завантаження форми
+        {
+            this.Text = "My Browser";                                                       // задання заголовка вікна програми
+        }
+
+        private void button1_Click(object sender, EventArgs e)                              // обробник події натискання на кнопку "Пошук"
+        {
+            webBrowser1.Navigate(textBox1.Text);                                            // перехід за URL-адресою, введеною у текстове поле
+        }
+
+        private void button2_Click(object sender, EventArgs e)                              // обробник події натискання на кнопку "Forward"
+        {
+            webBrowser1.GoForward();                                                        // перехід на сторінку вперед в історії браузера
+        }
+
+        private void button3_Click(object sender, EventArgs e)                              // обробник події натискання на кнопку "Back"
+        {
+            webBrowser1.GoBack();                                                           // перехід на сторінку назад в історії браузера
+        }
+
+        private void button4_Click(object sender, EventArgs e)                              // обробник події натискання на кнопку "Home"
+        {
+            webBrowser1.Navigate("youtube.com");                                            // перехід на сторінку youtube
+        }
+
+        private void button5_Click(object sender, EventArgs e)                              // обробник події натискання на кнопку "Save as..."
+        {
+            webBrowser1.Print();                                                            // виклик вікна збереження поточної веб-сторінки
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)             // обробник події зміни вибраного елемента у випадному списку
+        {
+            switch (comboBox1.SelectedIndex)                                                // перевірка індексу вибраного елемента
+            {
+                case 0: webBrowser1.Navigate("youtube.com"); break;                         // якщо вибрано перший пункт - перехід на YouTube
+                case 1: webBrowser1.Navigate("google.com"); break;                          // якщо вибрано другий пункт - перехід на Google
+                case 2: webBrowser1.Navigate("Gmail.com"); break;                           // якщо вибрано третій пункт - перехід на Gmail
+            }
+        }
+    }
+}
